7bd2dfd587060385dbf65c74715007241066b3ce
Борис <boris.krasilnikov98@gmail.com>
2020-02-17 14:07:25 +0300

    Добавьте файлы проекта.

---
+++ Currency/MyLib.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Xml;
+
+
+namespace MyLib
+{
+    public class Currency
+    {
+
+        //readonly
+        //dollar code R01235
+        public string CurrencyCode { get; private set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="currencyCode"></param>
+        public Currency(string currencyCode) => CurrencyCode = currencyCode;
+
+        /// <summary>
+        /// Курс по ЦБ Рф в диапозоне
+        /// </summary>
+        /// <param name="rangeTuple">Диапозон</param>
+        /// <returns></returns>
+        public IEnumerable<decimal> ValuesOfRange(DateTime FirsDateTime, DateTime LastDateTime)
+        {
+            List<decimal> values = new List<decimal>();
+            var xRoot = GetXmlElement(MakeUri(FirsDateTime,LastDateTime));
+            foreach (XmlNode xNode in xRoot)
+            {
+                foreach (XmlNode child in xNode)
+                {
+                    if (child.Name == "Value")
+                        values.Add(decimal.Parse(child.InnerText));
+                }
+            }
+            return values;
+        }
+
+
+        /// <summary>
+        /// Создаёт ссылку на ЦБ РФ
+        /// </summary>
+        /// <param name="FirstDate"></param>
+        /// <param name="SecondDate"></param>
+        /// <returns></returns>
+        private string MakeUri(DateTime FirstDate,DateTime SecondDate)
+        {
+            return "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=" +
+                   FirstDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture) + "&date_req2=" +
+                   SecondDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture) + "&VAL_NM_RQ="+CurrencyCode;
+        }
+
+        /// <summary>
+        /// Делает xmlElement из ссылки на xml файл
+        /// </summary>
+        /// <param name="Link"></param>
+        /// <returns></returns>
+        private XmlElement GetXmlElement(string Link)
+        {
+            var request = WebRequest.CreateHttp(Link);
+            var response = request.GetResponse();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(response.GetResponseStream());
+            return xDoc.DocumentElement;
+        }
+
+    }
+}
---
+++ DollarRate/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DollarRate
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        /// <summary>
+        /// Экземпляр графика
+        /// </summary>
+        public SeriesCollection DollarRateChart { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Событие на изменение даты в календаре
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Calendar_OnSelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime FirstDate = Calendar.SelectedDates.First();
+            DateTime LastDate = Calendar.SelectedDates.Last();
+            ChartBuilder(FirstDate,LastDate);
+        }
+
+
+        /// <summary>
+        /// Построение графика
+        /// </summary>
+        /// <param name="FDateTime">Первая дата</param>
+        /// <param name="LDateTime">Последняя дата</param>
+        public void ChartBuilder(DateTime FDateTime, DateTime LDateTime)
+        {
+            DataContext = null;
+            //Код доллара
+            MyLib.Currency currency = new MyLib.Currency("R01235");
+            DollarRateChart = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<decimal>(currency.ValuesOfRange(FDateTime, LDateTime))
+                },
+            };
+            DataContext = this;
+        }
+
+    }
+}
